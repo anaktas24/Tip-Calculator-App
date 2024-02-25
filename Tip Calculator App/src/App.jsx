7448a950eef7940ef7024e06f7768fb2aea98182
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-25 21:56:15 +0100

    function craetion

--- Tip Calculator App/src/App.jsx
+++ Tip Calculator App/src/App.jsx
@@ -49,7 +49,27 @@ function FriendList(){
 
 function Friend({friend}){
   return (
-    <li>{friend.name}</li>
+    <li>
+      <img src={friend.image} alt={friend.name}/>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owes you {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p >
+          You and your friend are even {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+
+    </li>
   )
 }
 
