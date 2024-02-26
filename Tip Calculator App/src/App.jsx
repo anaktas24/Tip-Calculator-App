ac579611ea8cb8c176d4d50cb3f4358d3177447d
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-26 21:38:24 +0100

    static part of forms

--- Tip Calculator App/src/App.jsx
+++ Tip Calculator App/src/App.jsx
@@ -32,6 +32,7 @@ function App() {
         <FormsFriend/>
         <Button>Add Friend</Button>
       </div>
+      <FormSplitBill/>
     </div>
   )
 }
@@ -87,6 +88,29 @@ function FormsFriend(){
       <label> Image URL</label>
       <input type="text"/>
       <Button>Add</Button>
+    </form>
+  )
+}
+
+function FormSplitBill(){
+  return(
+    <form className='form-split-bill'>
+      <h2>Split a bill with X</h2>
+      <label> Bill Value</label>
+      <input type="text"/>
+
+      <label> Your expense</label>
+      <input type="text"/>
+
+      <label> X expense</label>
+      <input type="text" disabled/>
+
+      <label> Who is paying</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Friend</option>
+      </select>
+      <Button>Split Bill</Button>
     </form>
   )
 }
