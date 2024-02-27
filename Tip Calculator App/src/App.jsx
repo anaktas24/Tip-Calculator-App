4bd2c9c1cb48699d0ea669b5ac91170214c958f1
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-27 21:43:42 +0100

    calc paying

--- Tip Calculator App/src/App.jsx
+++ Tip Calculator App/src/App.jsx
@@ -151,20 +151,32 @@ function FormsFriend({onAddFriend}){
 }
 
 function FormSplitBill(){
+  const [bill,setBill] = useState("")
+  const [paidByUser, setPaidByUser] = useState("")
+  const paidByFriend = bill ? bill - paidByUser : ""
+  const [whoIsPaying, setWhoIsPaying] = useState("user")
   return(
     <form className='form-split-bill'>
       <h2>Split a bill with {selectedFriend.name}</h2>
       <label> Bill Value</label>
-      <input type="text"/>
+      <input
+         type="text"
+         value={bill}
+         onChange={(e)=>setBill(Number(e.target.value))}/>
 
       <label> Your expense</label>
-      <input type="text"/>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e)=> setPaidByUser(e.target.value)}/>
 
       <label> {selectedFriend.name} expense</label>
-      <input type="text" disabled/>
+      <input type="text" disabled value={paidByFriend}/>
 
       <label> Who is paying</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e)=> setWhoIsPaying(e.target.value)}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
